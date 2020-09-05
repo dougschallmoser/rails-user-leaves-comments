604a41b8de66642710fc41966b4c4fd7e0dc9d04
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-05 07:44:52 -0700

    create resources for category, comment, post, user and post_category

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+
+    has_many :post_categories
+    has_many :categories, through: :post_categories
+    has_many :comments
+    has_many :users, through: :comments
+    
+end
