e2baf8a4305b655235aa2b587ad34b13c839fc0d
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-05 09:24:52 -0700

    update views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,16 @@
 class CategoriesController < ApplicationController
+
+    before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+    def index
+    end
+
+    def show
+    end
+
+    private
+
+    def set_category
+        @category = Category.find(params[:id])
+    end
 end
